c9f05a63b661d74d5ef7847a999b2667b482d732
VitorHmaia <vitorhmaia@outlook.com>
2023-11-27 19:55:28 -0300

    feat: display values in Brazilian currency format BRL

--- receitas-despesas/src/components/Transaction/Transaction.js
+++ receitas-despesas/src/components/Transaction/Transaction.js
@@ -1,15 +1,19 @@
 // Transaction.js
 import React from 'react';
 
-const Transaction = ({ transaction, deleteTransaction }) => {
-  const { id, description, amount } = transaction;
-
-  return (
-    <li key={id}>
-      {description}: R$ {amount}
-      <button onClick={() => deleteTransaction(id)}>Excluir</button>
-    </li>
-  );
-};
-
-export default Transaction;
+const formatCurrency = (value) => {
+    return value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  };
+  
+  const Transaction = ({ transaction, deleteTransaction }) => {
+    const { id, description, amount } = transaction;
+  
+    return (
+      <li key={id}>
+        {description}: {formatCurrency(amount)}
+        <button onClick={() => deleteTransaction(id)}>Excluir</button>
+      </li>
+    );
+  };
+  
+  export default Transaction;
--- receitas-despesas/src/components/TransactionList/TransactionList.js
+++ receitas-despesas/src/components/TransactionList/TransactionList.js
@@ -3,20 +3,20 @@ import React from 'react';
 import Transaction from '../Transaction/Transaction';
 
 const TransactionList = ({ transactions, deleteTransaction }) => {
-  return (
-    <div>
-      <h2>Lista de Transações</h2>
-      <ul>
-        {transactions.map((transaction) => (
-          <Transaction
-            key={transaction.id}
-            transaction={transaction}
-            deleteTransaction={deleteTransaction}
-          />
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default TransactionList;
+    return (
+      <div>
+        <h2>Lista de Transações</h2>
+        <ul>
+          {transactions.map((transaction) => (
+            <Transaction
+              key={transaction.id}
+              transaction={transaction}
+              deleteTransaction={deleteTransaction}
+            />
+          ))}
+        </ul>
+      </div>
+    );
+  };
+  
+  export default TransactionList;
