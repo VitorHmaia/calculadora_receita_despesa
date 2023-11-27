d8df142b4bb50a9f476434962aac5b4f4d5712d5
VitorHmaia <vitorhmaia@outlook.com>
2023-11-27 20:05:12 -0300

    feat: button to display the details of each item

--- receitas-despesas/src/components/Transaction/Transaction.js
+++ receitas-despesas/src/components/Transaction/Transaction.js
@@ -1,19 +1,42 @@
 // Transaction.js
-import React from 'react';
+import React, { useState } from 'react';
 
 const formatCurrency = (value) => {
-    return value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  return value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+};
+
+const TransactionDetails = ({ transaction, onClose }) => {
+  return (
+    <div>
+      <h3>Detalhes da Transação</h3>
+      <p><strong>Descrição:</strong> {transaction.description}</p>
+      <p><strong>Valor:</strong> {formatCurrency(transaction.amount)}</p>
+      <p><strong>Tipo:</strong> {transaction.type === 'income' ? 'Receita' : 'Despesa'}</p>
+      <p><strong>Tipo de Pagamento:</strong> {transaction.paymentType}</p>
+      <button onClick={onClose}>Fechar Detalhes</button>
+    </div>
+  );
+};
+
+const Transaction = ({ transaction, deleteTransaction }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const handleToggleDetails = () => {
+    setShowDetails(!showDetails);
   };
-  
-  const Transaction = ({ transaction, deleteTransaction }) => {
-    const { id, description, amount } = transaction;
-  
-    return (
-      <li key={id}>
-        {description}: {formatCurrency(amount)}
-        <button onClick={() => deleteTransaction(id)}>Excluir</button>
-      </li>
-    );
-  };
-  
-  export default Transaction;
+
+  return (
+    <li key={transaction.id}>
+      <span>
+        {transaction.description}: {formatCurrency(transaction.amount)}
+      </span>
+      <button onClick={handleToggleDetails}>Ver Detalhes</button>
+      {showDetails && (
+        <TransactionDetails transaction={transaction} onClose={handleToggleDetails} />
+      )}
+      <button onClick={() => deleteTransaction(transaction.id)}>Excluir</button>
+    </li>
+  );
+};
+
+export default Transaction;
